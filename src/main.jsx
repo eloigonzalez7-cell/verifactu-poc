ee473a4a9f65626d6c161ff73b0725a0570cd6ce
Yoloi <eloigonzalez@hotmail.com>
2025-10-05 18:52:30 +0200

    fixed styles import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import './index.css'
+import '../index.css'
 import { router } from './routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
